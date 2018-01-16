7714250577797816eb76b757674b49934c8b83fc
ricardo <ricardosantos.ti15@gmail.com>
2018-01-16 18:19:20 -0200

    add

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppComponent } from './app.component';
+import { BuscaCepModule } from './busca-cep/busca-cep.module';
+import { CepService } from './cep.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BuscaCepModule,
+    HttpModule
+  ],
+  providers: [CepService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/busca-cep/busca-cep.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CepService } from '../cep.service';
+import { Cep } from '../cep';
+
+@Component({
+  selector: 'app-busca-cep',
+  templateUrl: './busca-cep.component.html',
+  styleUrls: ['./busca-cep.component.css']
+})
+export class BuscaCepComponent implements OnInit {
+
+  cep = new Cep();
+  constructor( private cepService: CepService) { }
+
+  ngOnInit() {
+  }
+
+}
